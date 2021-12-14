345f692df1624eeb843a9c6641ad142dfc7116ae
coutinhoigor <81998304+coutinhoigor@users.noreply.github.com>
2021-12-14 00:09:26 -0300

    generate header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,10 +17,12 @@ import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { HeaderComponent } from './template/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
